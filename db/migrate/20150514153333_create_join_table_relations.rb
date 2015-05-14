509b38d744aebbcddeaf913a0364a87e0f522ed2
spectre6000 <spectre6000@gmail.com>
2015-05-14 09:40:38 -0600

    corrected relations in migrations and re-migrated

--- db/migrate/20150514153333_create_join_table_relations.rb
+++ db/migrate/20150514153333_create_join_table_relations.rb
@@ -1,6 +1,6 @@
 class CreateJoinTableRelations < ActiveRecord::Migration
   def change
-    add_reference :users, :attendee, references: :users, index: true, foreign_key: true
-    add_reference :events, :attended_event, references: :events, index: true, foreign_key: true
+    add_reference :invitations, :attendee, references: :users, index: true, foreign_key: true
+    add_reference :invitations, :attended_event, references: :events, index: true, foreign_key: true
   end
 end
