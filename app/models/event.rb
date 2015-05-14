a26e2107ec94d19821c890a40714318976afa1ba
spectre6000 <spectre6000@gmail.com>
2015-05-14 09:37:45 -0600

    add join relations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ActiveRecord::Base
   belongs_to :creator, :class_name => "User"
+  has_many :attendees, :foreign_key => 'attended_events'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   has_many :events, :foreign_key => 'creator_id'
+  has_many :attended_events, :foreign_key => 'attendee'
 end
